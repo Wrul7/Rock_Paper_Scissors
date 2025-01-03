8f0103a220438eefbc0c06effd91ef9dedc74d88
Russel <170238499+Wrul7@users.noreply.github.com>
2025-01-03 21:42:40 +0800

    Add result page with text elements

--- javascript.js
+++ javascript.js
@@ -17,21 +17,9 @@ function getComputerChoice() {
     }
 }
 
-// console.log(getComputerChoice());
-
-function getHumanChoice() {
-    let choice = prompt("Pick between the three:\nRock\nPaper\nScissors\nType your answer:");
-    return choice;
-}
-
-// console.log(getHumanChoice())
-
 function playRound(humanChoice, computerChoice, round) {
-    // console.log(String(round))
-
-    // humanChoice = humanChoice.toLowerCase();
-    // console.log("The player's choice: " + humanChoice);
-
+    
+    humanChoice = humanChoice.toLowerCase();
     computerChoice = computerChoice.toLowerCase();
     disp_computer_choice.textContent = "Computer picks " + getComputerChoice();
 
@@ -62,36 +50,21 @@ function playRound(humanChoice, computerChoice, round) {
             disp_round_winner.textContent = "You lose.";
             computerScore += 1;
         } else if (computerChoice == "paper") {
-            console.log("Computer chose paper. You win!");
+            disp_round_winner.textContent = "You win!";
             humanScore += 1;
         }
-    // } else {
-    //     console.log("Invalid input.")
+    }
 
     disp_player_score.textContent = player_name + ": " + humanScore;
     disp_computer_score.textContent = "Computer: " + computerScore;
-    }
 }
+
+// function winnerCheck() {
+
+// }
 
 let humanScore = 0;
 let computerScore = 0;
-
-// playRound(getHumanChoice(), getComputerChoice(), "Round 1")
-// playRound(getHumanChoice(), getComputerChoice(), "Round 2")
-// playRound(getHumanChoice(), getComputerChoice(), "Round 3")
-// playRound(getHumanChoice(), getComputerChoice(), "Round 4")
-// playRound(getHumanChoice(), getComputerChoice(), "Round 5")
-
-// Results
-
-console.log("Scores:\nComputer: " + String(computerScore) + "\nPlayer: " + String(humanScore));
-if (humanScore > computerScore) {
-    console.log("Congratulations, you win!")
-} else if (humanScore < computerScore) {
-    console.log("You lose. Computer wins.")
-} else if (humanScore == computerScore) {
-    console.log("It's a tie!")
-}
 
 const player_name = "Sample";
 let round_number = 0
@@ -104,19 +77,44 @@ const disp_computer_score = document.querySelector(".computer_score");
 const disp_round_number = document.querySelector(".round_number");
 const disp_computer_choice = document.querySelector(".computer_choice");
 const disp_round_winner = document.querySelector(".round_winner");
+const disp_instruction = document.querySelector(".instruction");
+const container_three = document.querySelector(".container_three");
+const container_three_one = document.querySelector(".container_three_one");
+const container_three_two = document.querySelector(".container_three_two");
 
 disp_player_name.textContent = "Hello, " + player_name + "!";
+disp_round_number.textContent = "Round " + round_number;
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener("click",  (e) => {
+        playRound(e.target.textContent, getComputerChoice(), round_number)
+        disp_round_number.textContent = "Round " + round_number;
+        round_number += 1;
+        if ((humanScore >= 5) || (computerScore >= 5)) {
+            toRemove = [container_three_one, container_three_two];
+            toRemove.forEach((elem) => {elem.remove();});
+            const winnerDisplay = document.createElement("h1");
+            container_three.appendChild(winnerDisplay);
+            if (humanScore >= 5) {
+                winnerDisplay.textContent = "You win!";
+            } else if (computerScore) {
+                winnerDisplay.textContent = "You lose."
+                const goodLuck = document.createElement("p");
+                container_three.appendChild(goodLuck);
+                goodLuck.textContent = "Better luck next time~"
+            }
+        }
+        // console.log(humanScore);
+        // console.log(computerScore);        
+    });
+});
+
+const htmlContainerThree = document.querySelector(".container_three");
 
 
-
-
-
-// const buttons = document.querySelectorAll("button");
-
-// buttons.forEach((button) => {
-//     button.addEventListener("click",  (e) => {
-//         playRound(e.target.textContent, getComputerChoice(), round_number)
-//         disp_round_number.textContent = "Round " + round_number;
-//         round_number += 1;
-//     })
-// });
+// if ((humanScore >= 5) || (computerScore >= 5)) {
+//     htmlContainerThree.remove();
+//     console.log("Yes");
+// }
